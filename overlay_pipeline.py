d415eb83a3a956ff9d9a12297e392ee5c6fbce24
Denys Kovalenko <denys.kovalenko@bolt.eu>
2022-11-05 02:08:40 +0200

    optimisation attempt, parametrise resoltion

--- overlay_pipeline.py
+++ overlay_pipeline.py
@@ -24,12 +24,20 @@ class TextConfig:
         self.color = color
 
 
+class TargetResolution:
+    def __init__(self, target_width: int, target_height: int) -> None:
+        self.target_width = target_width
+        self.target_height = target_height
+
+
 def add_transparent_image(background, foreground, x_offset=None, y_offset=None):
     bg_h, bg_w, bg_channels = background.shape
     fg_h, fg_w, fg_channels = foreground.shape
 
-    assert bg_channels == 3, f'background image should have exactly 3 channels (RGB). found:{bg_channels}'
-    assert fg_channels == 4, f'foreground image should have exactly 4 channels (RGBA). found:{fg_channels}'
+    if bg_channels != 3:
+        raise Exception(f'background image should have exactly 3 channels (RGB). found:{bg_channels}')
+    if fg_channels != 4:
+        raise Exception(f'foreground image should have exactly 4 channels (RGBA). found:{fg_channels}')
 
     # center by default
     if x_offset is None: x_offset = (bg_w - fg_w) // 2
@@ -62,30 +70,25 @@ def add_transparent_image(background, foreground, x_offset=None, y_offset=None):
     background[bg_y:bg_y + h, bg_x:bg_x + w] = composite
 
 
-''' ***** ***** ***** ***** ***** ***** *****
-Generate Video with Image Background -- Start
-***** ***** ***** ***** ***** ***** ***** '''
 
 
-def overlay_image(video_path, image_path, out_path, text_configs: List[TextConfig]):
+
+def overlay_image(video_path, image_path, out_path, text_configs: List[TextConfig],
+                  target_resolution: TargetResolution):
     video = cv2.VideoCapture(video_path)
-    image = cv2.imread(image_path)
-
-    def nothing():
-        pass
 
     # videowriter
     framespersecond = float(video.get(cv2.CAP_PROP_FPS))
     # todo can be smaller
-    res = (1920, 1080)
+    res = (target_resolution.target_width, target_resolution.target_height)
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     out = cv2.VideoWriter(out_path, fourcc, framespersecond, res)
 
 
-    overlay = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
-    # image = cv2.resize(image, (1920, 1080))
+    image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
+    overlay = cv2.resize(image, (target_resolution.target_height, target_resolution.target_width), fx=1, fy=1)
 
-    done = False;
+    done = False
 
     while not done:
         ret, video_frame = video.read()
@@ -94,9 +97,7 @@ def overlay_image(video_path, image_path, out_path, text_configs: List[TextConfi
             done = True
             continue
 
-        video_frame = cv2.resize(video_frame, (1920, 1080))
-
-
+        video_frame = cv2.resize(video_frame, (target_resolution.target_width, target_resolution.target_height))
         add_transparent_image(video_frame, overlay, 0, -20)
 
         im = Image.fromarray(cv2.cvtColor(video_frame, cv2.COLOR_BGR2RGB))
@@ -120,15 +121,19 @@ Generate Video with Image Background -- End
 if __name__ == "__main__":
     text_config1 = TextConfig("Hello, ee!",
                              "fonts/arial.ttf",
-                             48,
-                             370,
-                             952,
+                             36,
+                             200,
+                             300,
                               (0,0,0,0))
     text_config2 = TextConfig("Hello, Denys!",
                               "fonts/arial.ttf",
-                              48,
+                              36,
+                              300,
                               400,
-                              700,
                               (255,255,255,255))
 
-    overlay_image("video/2.mov", "video/Subject.png", "video/out.mp4", [text_config1, text_config2])
+    overlay_image("video/2.mov",
+                  "video/Subject.png",
+                  "video/out.mp4",
+                  [text_config1, text_config2],
+                  target_resolution=TargetResolution(target_width=850, target_height=480))
